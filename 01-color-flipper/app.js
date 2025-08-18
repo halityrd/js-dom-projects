8b420161de5389d511876957cce460204f0ba294
halityrd <halityrd@hotmail.com>
2025-08-18 14:22:22 +0300

    Simple Javascript

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,1 +1,19 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+  console.log(document.body);
+
+  // get random bumber between 0 - 3
+  const randomNumber = getRandomBumber();
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomBumber() {
+  return Math.floor(Math.random() * colors.length);
+}
