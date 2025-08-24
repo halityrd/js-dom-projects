79a694c6f5b3842af939e3869c5e1ae335a299b3
halityrd <halityrd@hotmail.com>
2025-08-24 23:16:32 +0300

    Smooth Scroll Complete

--- 10-scroll/index.js
+++ 10-scroll/index.js
@@ -9,14 +9,16 @@ const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', function () {
   //   linksContainer.classList.toggle('show-links');
-  const containerHeight = linksContainer.getBoundingClientRect().height;
+
   const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
 
   if (containerHeight === 0) {
     linksContainer.style.height = `${linksHeight}px`;
   } else {
     linksContainer.style.height = 0;
   }
+  // console.log(linksContainer.getBoundingClientRect());
 });
 
 // ********** fixed navbar ************
@@ -33,8 +35,11 @@ window.addEventListener('scroll', function () {
     navbar.classList.remove('fixed-nav');
   }
 
+  // setup back to top link
   if (scrollHeight > 500) {
     topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
   }
 });
 
@@ -42,18 +47,32 @@ window.addEventListener('scroll', function () {
 // select links
 const scrollLinks = document.querySelectorAll('.scroll-link');
 
-scrollLinks.forEach(function (link) {
+scrollLinks.forEach((link) => {
   link.addEventListener('click', function (e) {
     // prevent default
     e.preventDefault();
     // navigate to specific spot
     const id = e.currentTarget.getAttribute('href').slice(1);
     const element = document.getElementById(id);
-    let position = element.offsetTop;
+
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-nav');
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
     window.scrollTo({
       left: 0,
       top: position,
     });
+
+    // close
     linksContainer.style.height = 0;
   });
 });
