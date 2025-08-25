aa4a30f07868232a020976df99ad29e94a7ff328
halityrd <halityrd@hotmail.com>
2025-08-25 07:26:20 +0300

    Time From Now

--- 12-countdown-timer/index.js
+++ 12-countdown-timer/index.js
@@ -27,7 +27,13 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2020, 3, 25, 11, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+// let futureDate = new Date(2020, 3, 25, 11, 30, 0);
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -77,7 +83,7 @@ function getRemainingTime() {
   }
 
   items.forEach(function (item, index) {
-    item.innerHTML = values[index];
+    item.innerHTML = format(values[index]);
   });
 
   if (t < 0) {
