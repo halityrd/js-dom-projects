061f969e247fee03947820171964856d56066e90
halityrd <halityrd@hotmail.com>
2025-08-27 01:45:51 +0300

    addItem Setup

--- 14-grocery-bud/index.js
+++ 14-grocery-bud/index.js
@@ -11,3 +11,21 @@ const clearBtn = document.querySelector('.clear-btn');
 let editElement;
 let editFlag = false;
 let editID = '';
+
+// ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener('submit', addItem);
+
+// ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+  if ((value !== '') & (editFlag === false)) {
+    console.log('add item to the list');
+  } else if (value !== '' && editFlag === true) {
+    console.log('editing');
+  } else {
+    console.log('empty value');
+  }
+}
