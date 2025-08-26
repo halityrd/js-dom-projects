971ec44159e0fd1e59a82632d630b6317b4fdc8f
halityrd <halityrd@hotmail.com>
2025-08-27 02:44:36 +0300

    Clear Items

--- 14-grocery-bud/index.js
+++ 14-grocery-bud/index.js
@@ -15,6 +15,8 @@ let editID = '';
 // ****** EVENT LISTENERS **********
 // submit form
 form.addEventListener('submit', addItem);
+// clear items
+clearBtn.addEventListener('click', clearItems);
 
 // ****** FUNCTIONS **********
 function addItem(e) {
@@ -70,9 +72,28 @@ function displayAlert(text, action) {
   }, 1000);
 }
 
+// clear items
+function clearItems() {
+  const items = document.querySelectorAll('.grocery-items');
+
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      list.removeChild(item);
+    });
+  }
+
+  container.classList.remove('show-container');
+  displayAlert('empty list', 'danger');
+  setBackToDefault();
+  //   localStorage.removeItem('list');
+}
+
 // set back to default
 function setBackToDefault() {
-  console.log('set back to default');
+  grocery.value = '';
+  editFlag = false;
+  editID = '';
+  submitBtn.textContent = 'submit';
 }
 
 // ****** LOCAL STORAGE **********
