02059fcc747ce85017f76a9e781198e2ff2b0e32
halityrd <halityrd@hotmail.com>
2025-08-27 03:18:33 +0300

    Delete Item

--- 14-grocery-bud/index.js
+++ 14-grocery-bud/index.js
@@ -44,7 +44,7 @@ function addItem(e) {
             </div>
           `;
     const deleteBtn = element.querySelector('.delete-btn');
-    const editBtn = element.querySelector('.eidt-btn');
+    const editBtn = element.querySelector('.edit-btn');
     deleteBtn.addEventListener('click', deleteItem);
     editBtn.addEventListener('click', editItem);
     // append child
@@ -89,12 +89,22 @@ function clearItems() {
   container.classList.remove('show-container');
   displayAlert('empty list', 'danger');
   setBackToDefault();
+  // remove from local storage
   //   localStorage.removeItem('list');
 }
 
 // delete function
-function deleteItem() {
-  console.log('item deleted');
+function deleteItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  list.removeChild(element);
+  if (list.children.length === 0) {
+    container.classList.remove('show-container');
+  }
+  displayAlert('item removed', 'danger');
+  setBackToDefault();
+  // remove from local storage
+  removeFromLocalStorage(id);
 }
 
 // edit function
@@ -114,3 +124,5 @@ function setBackToDefault() {
 function addToLocalStorage(id, value) {
   console.log('added to local storage');
 }
+
+function removeFromLocalStorage(id) {}
