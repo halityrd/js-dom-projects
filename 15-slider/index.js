7f63facd74681ab3e27d21ca4d4f3a10bdef04ff
halityrd <halityrd@hotmail.com>
2025-08-27 20:56:32 +0300

    Slides Approach

--- 15-slider/index.js
+++ 15-slider/index.js
@@ -5,3 +5,31 @@ const prevBtn = document.querySelector('.prevBtn');
 slides.forEach(function (slide, index) {
   slides.style.left = `${index * 100}%`;
 });
+
+let counter = 0;
+
+nextBtn.addEventListener('click', function () {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener('click', function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  // working with slides
+
+  if (counter === slides.length) {
+    counter = slides.length - 1;
+  }
+
+  if (counter < 0) {
+    counter = slides.length - 1;
+  }
+
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100})`;
+  });
+}
